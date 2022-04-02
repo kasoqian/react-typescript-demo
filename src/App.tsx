bb2a676a45ce621b8de5f114502a1ae6954dc367
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-04-02 23:44:02 +0800

    feat: 学完了props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+interface AppProps {
+  headerText: string;
+  // 可选操作符，意思为该参数可选传
+  extraText?: string;
+}
+// 可以直接设置默认参数，并定义他的类型
+function App({ headerText, extraText = "默认参数，extraText" }: AppProps) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{headerText}</h1>
+      {extraText && <p>{extraText}</p>}
     </div>
   );
 }
